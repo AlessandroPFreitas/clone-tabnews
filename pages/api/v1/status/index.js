3dfc12f31b2ed2706157cfc28342de4963f774f4
Alessandro Paulista Freitas <alessandropf2000@gmail.com>
2026-02-11 20:32:33 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
-import database from "@/infra/database.js"
+import database from "infra/database.js"
 
 const status = async (request, response) => {
-  const result = await database.query("SELECT 1 + 1 AS result")
-  console.log(result.rows)
-  response.status(200).json({ status: "acima da média" })
+  const updatedAt = new Date().toISOString()
+  const maxConnections = await database.query("SHOW max_connections;")
+
+  const dbName = process.env.POSTGRES_DB
+  const usedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  })
+  const usedConnectionsCount = usedConnections.rows[0].count
+  const versionPostgresql = await database.query("SHOW server_version;")
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version_postgresql: versionPostgresql.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        used_connections: parseInt(usedConnectionsCount),
+      },
+    },
+  })
 }
 
 export default status
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,4 +2,13 @@ test("GET to api/v1/status should return 200", async () => {
   const url = "http://localhost:3000/"
   const response = await fetch(`${url}api/v1/status`)
   expect(response.status).toBe(200)
+
+  const responseBody = await response.json()
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
+  expect(parsedUpdatedAt).toBe(responseBody.updated_at)
+
+  expect(responseBody.dependencies.database.used_connections).toEqual(1)
+  expect(responseBody.dependencies.database.max_connections).toEqual(100)
+  expect(responseBody.dependencies.database.version_postgresql).toEqual("16.0")
 })
